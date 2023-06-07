e982304ecaf2817072aa07e3c4b6d4cef0a52fd4
Asfandyar-Khan-2022 <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2023-06-07 02:40:01 +0100

    following pep8 standards

--- resize_image.py
+++ resize_image.py
@@ -1,21 +1,37 @@
+"""Resize image
+
+Module to resize images in a folder by its width
+
+Typical usage example:
+
+    resize(folder directory, integer pixel width)
+"""
+
 from PIL import Image
 import os
 
 
-def resize(im, new_width):
-    width, height = im.size
-    ratio = height/width
-    new_height = int(ratio*new_width)
-    resized_img = im.resize((new_width, new_height))
-    return resized_img
+def resize(dir, new_width):
+    """Changes image width with a fixed ratio
 
-files = os.listdir('finger_images')
-extentions = ['jpg', 'jped', 'png', 'gif']
-for file in files:
-    ext = file.split('.')[-1]
-    if ext in extentions:
-        im = Image.open('finger_images/'+file)
-        im_resized = resize(im,200)
-        filepath = f'finger_images/{file}'
-        im_resized.save(filepath)
+    Args:
+        dir: Directory of folder containing images.
+        new_width: New pixel width of the images.
 
+    Raises:
+        FileNotFoundError: If file path is not specified.
+    """
+    files = os.listdir(dir)
+    extentions = ['jpg', 'jped', 'png', 'gif']
+
+    for file in files:
+        ext = file.split('.')[-1]
+
+        if ext in extentions:
+            im = Image.open(dir+'/'+file)
+            width, height = im.size
+            ratio = height/width
+            new_height = int(ratio*new_width)
+            im_resized = im.resize((new_width, new_height))
+            filepath = f'{dir}/{file}'
+            im_resized.save(filepath)
